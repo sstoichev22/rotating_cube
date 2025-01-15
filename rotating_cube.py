70045930b8c12ffe8bcbb1a83683dc4b88fb16ac
stefan stoichev <stefanhstoichev@gmail.com>
2025-01-15 01:03:49 -0800

    added individual speeds and thetas for each angle, separated out the rotation matrixs for each component(x, y, z) and added on screen text for the x, y, and z thetas, the terminal shows the speeds, change which dir you want to rotate by pressing x, y, or z then using arrow keys to increase/decrease speed

--- rotating_cube.py
+++ rotating_cube.py
@@ -3,8 +3,10 @@ import pygame
 import math
 
 pygame.init()
+pygame.font.init()
 
 screen = pygame.display.set_mode([1280, 720])
+font = pygame.font.SysFont('Arial', 30)
 
 cube3d = [(1, 1, 1), 
         (1, 1, -1), 
@@ -30,30 +32,55 @@ edges = [(0, 1),
 
 
 
-def rotate_point(x, y, z):
+def rotate_x(x, y, z):
     x_rot = x
-    y_rot = y * math.cos(theta) - z * math.sin(theta)
-    z_rot = y * math.sin(theta) + z * math.cos(theta)
-    
-    x_final = x_rot * math.cos(theta) - y_rot * math.sin(theta)
-    y_final = x_rot * math.sin(theta) + y_rot * math.cos(theta)
-    z_final = z_rot
+    y_rot = y * math.cos(xtheta) - z * math.sin(xtheta)
+    z_rot = y * math.sin(xtheta) + z * math.cos(xtheta)
+    return x_rot, y_rot, z_rot
 
-    return x_final, y_final, z_final
+def rotate_y(x, y, z):
+    x_rot = x * math.cos(ytheta) + z * math.sin(ytheta)
+    y_rot = y
+    z_rot = z * math.cos(ytheta) - x * math.sin(ytheta)
+    return x_rot, y_rot, z_rot
+
+def rotate_z(x, y, z):
+    x_rot = x * math.cos(ztheta) - y * math.sin(ztheta)
+    y_rot = x * math.sin(ztheta) + y * math.cos(ztheta)
+    z_rot = z
+    return x_rot, y_rot, z_rot
 
 def update():
     screen.fill((0, 0, 0))
 
-    global theta
-    theta = (theta + speed)# + (2*math.pi/(360*2)))
+    global xtheta, ytheta, ztheta
+
+    xtheta = (xtheta+xspeed)%(2*math.pi)
+
+    ytheta = (ytheta+yspeed)%(2*math.pi)
+
+    ztheta = (ztheta+zspeed)%(2*math.pi)
+    
     cube2d = []
     for i in range(len(cube3d)):
-        x_rot, y_rot, z_rot = rotate_point(cube3d[i][0], cube3d[i][1], cube3d[i][2])
+        x_rot, y_rot, z_rot = rotate_x(*rotate_y(*rotate_z(cube3d[i][0], cube3d[i][1], cube3d[i][2])))
         cube2d.append(((x_rot * fov / (fov + z_rot)) * 100 + screen.get_width() / 2,
                        (y_rot * fov / (fov + z_rot)) * 100 + screen.get_height() / 2))
 
     for i in range(len(edges)):
         pygame.draw.line(screen, (255, 255, 255), cube2d[edges[i][0]], cube2d[edges[i][1]])
+
+    currvar_text = font.render(currvar, True, (255, 255, 255))
+    screen.blit(currvar_text, (3*screen.get_width()/4, screen.get_height()/2))
+
+    x_text = font.render("x:%.5f"%xtheta, True, (255, 255, 255))
+    screen.blit(x_text, (3*screen.get_width()/4, screen.get_height()/2+50))
+
+    y_text = font.render("y:%.5f"%ytheta, True, (255, 255, 255))
+    screen.blit(y_text, (3*screen.get_width()/4, screen.get_height()/2+100))
+
+    z_text = font.render("z:%.5f"%ztheta, True, (255, 255, 255))
+    screen.blit(z_text, (3*screen.get_width()/4, screen.get_height()/2+150))
 
     pygame.display.update()
 
@@ -67,8 +94,9 @@ delta = 0
 fps = 1/60
 frames = 0
 fov = 10
-theta = 0
-speed = 0
+xtheta, ytheta, ztheta = 0, 0, 0
+xspeed, yspeed, zspeed = 0, 0, 0
+currvar = 'x'
 
 while running:
     for event in pygame.event.get():
@@ -88,12 +116,30 @@ while running:
 
     keys = pygame.key.get_pressed()
     if(keys[pygame.K_LEFT]):
-        speed -= fps/1000
+        if currvar == 'x':
+            xspeed -= fps/1000
+        if currvar == 'y':
+            yspeed -= fps/1000
+        if currvar == 'z':
+            zspeed -= fps/1000
     if(keys[pygame.K_RIGHT]):
-        speed += fps/1000
-    print(speed)
+        if currvar == 'x':
+            xspeed += fps/1000
+        if currvar == 'y':
+            yspeed += fps/1000
+        if currvar == 'z':
+            zspeed += fps/1000
+    if(keys[pygame.K_x]):
+        currvar = 'x'
+    if(keys[pygame.K_y]):
+        currvar = 'y'
+    if(keys[pygame.K_z]):
+        currvar = 'z'
+    print("x:%f"%xspeed,"\ny:%f"%yspeed,"\nz:%f"%zspeed)
 
-    speed += speed/30000*-1
+    xspeed += xspeed/75000*-1
+    yspeed += yspeed/75000*-1
+    zspeed += zspeed/75000*-1
 
 
 pygame.quit()
